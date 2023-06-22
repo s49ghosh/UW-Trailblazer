74bf2b008906efd34f0475c155c5424d2408f3cf
Helen Xia <58539579+helenxiia@users.noreply.github.com>
2023-06-21 21:41:02 -0400

    rewrote CREATE TABLES to match schema

--- backend/database/sql/createtables.sql
+++ backend/database/sql/createtables.sql
@@ -1,50 +1,68 @@
 -- Creates tables for the dataset
 -- Creates relevent schemas and datatypes for each column
 
-
-CREATE TABLE term (
-    term_id INT NOT NULL PRIMARY KEY,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    term_date DATE NOT NULL,
-    additional_info TEXT
-);
-
-CREATE TABLE subject (
-    subject_code INT PRIMARY KEY NOT NULL,
-    subject_name TEXT NOT NULL,
-    num_of_courses INT NOT NULL,
-    course_level CHAR NOT NULL,
-    avg_rating DECIMAL(3,2) NOT NULL
-);
-
-CREATE TABLE rating (
-    course_code INT PRIMARY KEY NOT NULL,
-    rating INT NOT NULL,
-    uid INT NOT NULL,
-    FOREIGN KEY (uid) REFERENCES user (uid)
-);
-
-CREATE TABLE courses (
-    course_code INT PRIMARY KEY NOT NULL,
-    course_name TEXT NOT NULL,
-    subject_code INT,
-    rating INT,
-    enroll_cap INT NOT NULL,
-    term_avail INT NOT NULL,
-    prereqs TEXT,
-    leads_to INT,
-    anti_reqs TEXT,
-    FOREIGN KEY (subject_code) REFERENCES subject (subject_code),
-    FOREIGN KEY (leads_to) REFERENCES courses (course_code)
-);
-
-CREATE TABLE user (
+CREATE TABLE Users (
     uid INT PRIMARY KEY NOT NULL,
     first_name TEXT,
     last_name TEXT,
-    friends TEXT,
-    start_year INT NOT NULL,
-    taken_courses TEXT,
-    planned_courses TEXT
+);
+
+
+CREATE TABLE Subjects (
+    subject_code VARCHAR(10) PRIMARY KEY NOT NULL,
+    subject_name VARCHAR(100),
+    num_courses INT,
+    avg_rating DECIMAL(3,2)
+    
+);
+
+CREATE TABLE Terms (
+    term_id INT PRIMARY KEY NOT NULL,
+    start_date DATE,
+    end_date DATE,
+    term_season VARCHAR(10)
+);
+
+
+CREATE TABLE Courses (
+    course_code VARCHAR(10) PRIMARY KEY NOT NULL,
+    course_name VARCHAR(100),
+    subject_code VARCHAR(10),    
+    course_level INT,
+    enroll_cap INT,
+    FOREIGN KEY (subject_code) REFERENCES Subjects(subject_code)
+);
+
+CREATE TABLE UserFriends (
+    uid INT NOT NULL,
+    friend_id INT NOT NULL,
+    PRIMARY KEY (uid, friend_id),
+    FOREIGN KEY (uid) REFERENCES Users(uid),
+    FOREIGN KEY (friend_id) REFERENCES Users(uid)
+);
+
+CREATE TABLE UserPlannedCourses (
+    uid INT,
+    course_code VARCHAR(10),
+    PRIMARY KEY (uid, course_code),
+    FOREIGN KEY (uid) REFERENCES Users(uid),
+    FOREIGN KEY (course_code) REFERENCES Courses(course_code)
+);
+
+
+CREATE TABLE UserTakenCourses (
+    uid INT,
+    course_code VARCHAR(10),
+    PRIMARY KEY (uid, course_code),
+    FOREIGN KEY (uid) REFERENCES Users(uid),
+    FOREIGN KEY (course_code) REFERENCES Courses(course_code)
+);
+
+
+CREATE TABLE Ratings (
+    course_code VARCHAR(10) PRIMARY KEY NOT NULL,
+    rating INT NOT NULL,
+    uid INT NOT NULL,
+    FOREIGN KEY (uid) REFERENCES Users(uid),
+    FOREIGN KEY (course_code) REFERENCES Courses(course_code)
 );

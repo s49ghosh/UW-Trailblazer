e97484f8b6356d1baadf6ce521a716687a16c4bf
jasminexu1 <jasminexu0813@gmail.com>
2023-07-06 04:10:39 -0400

    changed credentials

--- productionDB/call_api.py
+++ productionDB/call_api.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 # Configure MySQL
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Jingman20020813'
-app.config['MYSQL_DB'] = 'testdb'
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # Initialize MySQL
